88cb5836481c3e9813e1e3aff88bad61a43d85fe
Sameer Ahmad <sameer1.ahmad@ril.com>
2021-03-24 13:55:42 +0530

    Intial Setup

---
+++ src/com/example/tutorials/programs/StringReverse.java
@@ -0,0 +1,45 @@
+package com.example.tutorials.programs;
+
+public class StringReverse {
+    public static void main(String[] args) {
+
+        System.out.println(reverseStringUsingStringBuilder("Hello"));
+        System.out.println(reverseStringNormal("Hello"));
+        System.out.println(reverseString("Hello"));
+    }
+    /**
+     * takes String param to process
+     * it is a bad practice for taking help of string class object doing the operation in this way
+     * this process makes so many objects and garbage collected
+     * */
+    private static String reverseString(String str) {
+        String str1 = "";
+        for (int i = str.length() -1 ; i >= 0 ; i--) {
+            str1 = str1 + str.charAt(i);
+        }
+        return str1;
+    }
+    /**
+     * takes String param to process
+     * this method process each character from the string and append to string builder
+     * to avoid multiple objects creation
+     * */
+    private static String reverseStringNormal(String str) {
+        StringBuilder strBuilder = new StringBuilder();
+        for (int i = str.length() -1 ; i >= 0 ; i--) {
+            strBuilder.append(str.charAt(i));
+        }
+        return strBuilder.toString();
+    }
+
+
+    /**
+     * takes String param to process
+     * this method is uses string builder inbuilt method to reverse the string
+     * */
+    private static String reverseStringUsingStringBuilder(String str) {
+        return new StringBuilder(str)
+                .reverse()
+                .toString();
+    }
+}
